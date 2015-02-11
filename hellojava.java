fdacc9d60ee46ecba02c6551a9500251d630c449
descartez <rvanstong@gmail.com>
2015-02-11 15:45:57 -0800

    adds user input capture

--- hellojava.java
+++ hellojava.java
@@ -12,8 +12,10 @@ public class hellojava {
       console.printf("    || Arcade ||\n");
       console.printf("    ------------\n\n");
       console.printf("    ----Games---\n");
-      console.printf("-Select By Typing Number-\n\n");
       console.printf("1. Rock, Paper, Scissors\n");
-      console.printf("   --More to come!--\n\n\n");
+      console.printf("2. Mad Libs\n");
+      console.printf("   --More to come!--\n\n");
+      console.printf("-Select By Typing Number-\n");
+      String userResponse = console.readLine("> ");
     }
 }
