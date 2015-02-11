231e0e69daab6b616a10fab7b78d5b10d6b94176
descartez <rvanstong@gmail.com>
2015-02-11 15:37:58 -0800

    adds in game selection interface

--- hellojava.java
+++ hellojava.java
@@ -1,6 +1,18 @@
+import java.io.Console;
+
 public class hellojava {
 
     public static void main(String []args) {
-       System.out.println("Hello World");
+      Console console = System.console();
+      console.printf("Welcome to the...\n");
+      console.printf("    ____________\n");
+      console.printf("    ||  Hello ||\n");
+      console.printf("    ||  Java  ||\n");
+      console.printf("    || Arcade ||\n");
+      console.printf("    ------------\n\n");
+      console.printf("    ----Games---\n");
+      console.printf("-Select By Typing Number-\n\n");
+      console.printf("1. Rock, Paper, Scissors\n");
+      console.printf("   --More to come!--\n\n\n");
     }
 }
