ec8a8bb86cd8db15ebdf4af673b8c403fc4ffa48
descartez <rvanstong@gmail.com>
2015-02-11 15:55:31 -0800

    adds in Mad Lib game

--- hellojava.java
+++ hellojava.java
@@ -2,28 +2,48 @@ import java.io.Console;
 
 public class hellojava {
 
-    public static void main(String []args) {
-      Console console = System.console();
+  public static void main(String []args) {
+    Console console = System.console();
 // -------GAME SELECTION MENU------------
-      console.printf("Welcome to the...\n");
-      console.printf("    ____________\n");
-      console.printf("    ||  Hello ||\n");
-      console.printf("    ||  Java  ||\n");
-      console.printf("    || Arcade ||\n");
-      console.printf("    ------------\n\n");
-      console.printf("    ----Games---\n");
-      console.printf("1. Mad Libs\n");
-      console.printf("2. Rock, Paper, Scissors (WIP)\n");
-      console.printf("   --More to come!--\n\n");
-      console.printf("-Select By Typing Number-\n");
+    console.printf("Welcome to the...\n");
+    console.printf("    ____________\n");
+    console.printf("    ||  Hello ||\n");
+    console.printf("    ||  Java  ||\n");
+    console.printf("    || Arcade ||\n");
+    console.printf("    ------------\n\n");
+    console.printf("    ----Games---\n");
+    console.printf("1. Mad Libs\n");
+    console.printf("2. Rock, Paper, Scissors (WIP)\n");
+    console.printf("   --More to come!--\n\n");
+    console.printf("-Select By Typing Number-\n");
 
       // grab the user input
-      String userResponseAsString = console.readLine("> ");
+    String userResponseAsString = console.readLine("> ");
       // parse it into a number
-      int userResponseAsInt = Integer.parseInt(userResponseAsString);
+    int userResponseAsInt = Integer.parseInt(userResponseAsString);
 
-      console.printf(userResponseAsString + "\n");
-      console.printf(userResponseAsInt + "\n");
+    console.printf(userResponseAsString + "\n");
+    console.printf(userResponseAsInt + "\n");
 
+      // ---------Mad Libs Game---------
+    if (userResponseAsInt == 1) {
+      console.printf("You chose Mad Libs! Excellent choice!\n");
+      String name = console.readLine("Enter your name:  ");
+      String adjective = console.readLine("Enter an adjective:  ");
+      String noun = console.readLine("Enter a noun:  ");
+      boolean censorWord = (noun.equalsIgnoreCase("dork") ||
+        noun.equalsIgnoreCase("jerk"));
+      do {
+        if (censorWord) {
+          console.printf("Watch your fucking language poonslayer. Try again\n\n");
+        };
+      } while(censorWord);
+
+      String adverb = console.readLine("Enter an adverb:  ");
+      String verb = console.readLine("Enter a verb ending in -ing:  ");
+
+      console.printf("%s is a %s %s. ", name, adjective, noun);
+      console.printf("They are always %s %s.\n", adverb, verb);
     }
+  }
 }
